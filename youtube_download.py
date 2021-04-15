9830d3b94d4cad43262b41f281631b82b9680a31
Pigi-bytes <derouaultbarthelemy@gmail.com>
2021-04-15 02:48:43 +0200

    Rename file

--- youtube_download.py
+++ youtube_download.py
@@ -25,8 +25,8 @@ class Ui(QtWidgets.QMainWindow):
         uic.loadUi("Ui/youtubeDownload.ui", self)
         # We load the .ui file into the class
 
-        # we found the widgets and store it into var,
-        # We also associate the button and theirs function
+        # we found the widgets and store them into var,
+        # We also associate the button and their functions
         # ----------------------------------------------------------------------
         self.ok_button = self.findChild(QtWidgets.QPushButton, "ok")
         self.ok_button.clicked.connect(self.button_ok_press)
@@ -46,11 +46,11 @@ class Ui(QtWidgets.QMainWindow):
 
     def button_ok_press(self):
         self.error_in_thread = False
-        # If a error occur in the thread we store it in this variable 
+        # If an error occurs in the thread we store it in this variable 
         threading.Thread(target=self.Thread_for_get_data).start()
         # We start the Thread 
         if self.error_in_thread == True:
-            # if the var is set to TRUE in the thread , that mean we have a error
+            # if the var is set to TRUE in the thread , that means we have an error
             # and that our url is not valid 
             QMessageBox.critical(
             self,
@@ -61,15 +61,15 @@ class Ui(QtWidgets.QMainWindow):
 
     def Thread_for_get_data(self):
         """
-        Here, we watch is a url is valide, and if it is, wa update the GUI, title and  thumbnail 
+        Here, we watch if a url is valid, and if it is, we update the GUI, title and thumbnail 
         """
         self.youtube_url = self.input.text()
         # we store the url in a more readable name
         try:
-            # If a error occure here, that mean that the url is not valide
+            # If an error occurs here, that means that the url is not valide
 
             self.video = pytube.YouTube(self.youtube_url)
-            # We create the video objet, who containe the video we want
+            # We create the video objet, which contains the video we want
             # we use that object for all of the downloading process
 
             self.titre_yt = self.video.title
@@ -78,13 +78,12 @@ class Ui(QtWidgets.QMainWindow):
             ID = self.youtube_url[-11:]
             thumbnail_link = f"http://img.youtube.com/vi/{ID}/hqdefault.jpg"
             # We create the url of the thumbnail.
-            # first of all we take the ID of the youtube video that the last 11 character.
-            # We add that in a url and we can now have the link of the picture use for the thumbnail,
+            # first of all we take the ID of the youtube video, the last 11 character.
+            # We add that in a url and we can now have the link of the picture used for the thumbnail,
             # is a 480 x 360 px, jpg. => https://yt-thumb.canbeuseful.com/en
 
             data = urllib.request.urlopen(thumbnail_link).read()
-            # we catch the data of the image into the memory bu doing a request,
-            # That is what freeze the Gui..
+            # we catch the data of the image into the memory by doing a request,
 
             image = QImage()
             image.loadFromData(data)
@@ -95,7 +94,7 @@ class Ui(QtWidgets.QMainWindow):
             self.download.setDisabled(False)
             # We activate the download Button
         except:
-            self.scotch = True
+            self.error_in_thread = True
             # if something went wrong with the url
 
     def download_button_press(self):
@@ -104,20 +103,20 @@ class Ui(QtWidgets.QMainWindow):
         it is deactivated if the url is not valid
         """
         self.folderpath = QtWidgets.QFileDialog.getExistingDirectory(self, "Select Folder")
-        # We get the Path where the user want to download is file
+        # We get the Path where the user wants to download his file
         if self.folderpath != '':
-            # We Check if the user dosen't have close the window for choosing his file 
+            # We Check if the user hasn't closed the window for choosing his file 
             self.loadbar = ProgressBar(100, title=f"{self.titre_yt} download in progress ")
-            # We create a instance of the class ProgressBar. It is for showing the user how many time left
+            # We create an instance of the class ProgressBar. It is for showing the user how much time is left
             self.video.register_on_progress_callback(self.progress_func)
-            # We say that, for each chunk of data retrived,when downloading,we call that function
+            # We say that, for each chunk of data retrieved,when downloading,we call that function
             self.lastprogress = 0
-            # last purcentage of the download => ssee below, that act like a reset
+            # last percentage of the download => see below, that acts like a reset
             if self.audio_only.isChecked():
-                # if the checkbox "audio only" is check
+                # if the checkbox "audio only" is checked
                 self.video.streams.filter(only_audio=True).first().download(self.folderpath)
-                # We download the video by filtering all of the track, and we only keep the audio.
-                # That save the file in a .mp4 format
+                # We download the video by filtering all of the tracks, and we only keep the audio.
+                # That saves the file in a .mp4 format
                 threading.Thread(target=self.Thread_for_conversion).start()
                 # we call a thread for converting the video into audio, bc if the video is huge, the process 
                 # while take a while and halt the GUI
@@ -132,7 +131,7 @@ class Ui(QtWidgets.QMainWindow):
                 # that save the file in a .mp4 format
 
                 MessageBeep(-1)
-                # do a bip 
+                # does a bip 
 
     def progress_func(self, stream, chunk, bytes_remaining):
         """
@@ -143,10 +142,10 @@ class Ui(QtWidgets.QMainWindow):
         bytes_downloaded = total_size - bytes_remaining
         # we get the total of bytes downloaded
         liveprogress = int(bytes_downloaded / total_size * 100)
-        # we make a purcentage of that
+        # we make a percentage of that
         if liveprogress > self.lastprogress:
-            # if the pourcentage is greater that the last purcentage,
-            # is for making it less buggy and more smooth
+            # if the pourcentage is greater that the last percentage,
+            # is for making it less buggy and smoother
             self.loadbar.setValue(liveprogress)
             # we update the progress bar
         self.lastprogress = liveprogress
@@ -165,13 +164,13 @@ class Ui(QtWidgets.QMainWindow):
                         # we call subprocess for doing a command that will convert
                         # .mp4 audio file into .mp3 
         os.remove(filePath + '.mp4')
-        # we delete the .mp4 file create in the first place
+        # we delete the .mp4 file created in the first place
         MessageBeep(-1)
-        # do a bip 
+        # does a bip 
 
     def resource_path(self, relative_path):
         """
-        Get the path for a bin executable 
+        Get the path for executable bin  
         """
         if hasattr(sys, '_MEIPASS'):
             return os.path.join(sys._MEIPASS, relative_path)
@@ -200,8 +199,8 @@ class ProgressBar(QProgressDialog):
 if __name__ == "__main__":
     # Create an instance of QApplication
     app = QtWidgets.QApplication(sys.argv)
-    # Show the calculator's GUI
+    # Show the downloader's GUI
     window = Ui()
     window.show()
-    # Execute the calculator's main loop
+    # Execute the downloader's main loop
     sys.exit(app.exec_())
