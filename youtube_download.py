f5d53398e13eab283dcb3cb62853d65bc516778a
Pigi-bytes <derouaultbarthelemy@gmail.com>
2021-04-20 20:56:42 +0200

    fix merge

--- youtube_download.py
+++ youtube_download.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-import os, sys, shutil, subprocess
+import os
+import sys
+import shutil
+import subprocess
 import re
 import urllib
 import threading
@@ -26,7 +29,8 @@ class Ui(QtWidgets.QMainWindow):
         uic.loadUi("Ui/youtubeDownload.ui", self)
         # We load the .ui file into the class
 
-        self.reset_temp_file() 
+        self.path_temp = Path(os.getcwd(), "Temp")
+        self.reset_temp_file()
 
         # we found the widgets and store them into var,
         # We also associate the button and their functions
@@ -49,22 +53,22 @@ class Ui(QtWidgets.QMainWindow):
 
     def button_ok_press(self):
         self.error_in_thread_ok = False
-        # If an error occurs in the thread we store it in this variable 
+        # If an error occurs in the thread we store it in this variable
         threading.Thread(target=self.Thread_for_get_data).start()
-        # We start the Thread 
+        # We start the Thread
         if self.error_in_thread_ok == True:
             # if the var is set to TRUE in the thread , that means we have an error
-            # and that our url is not valid 
+            # and that our url is not valid
             QMessageBox.critical(
             self,
             "Error",
-            """An error occured, did you copy your url correctly? 
+            """An error occured, did you copy your url correctly?
             Is it valid? Is it in the right format?""",)
             # we show the user the message of error
 
     def Thread_for_get_data(self):
         """
-        Here, we watch if a url is valid, and if it is, we update the GUI, title and thumbnail 
+        Here, we watch if a url is valid, and if it is, we update the GUI, title and thumbnail
         """
         self.youtube_url = self.input.text()
         # we store the url in a more readable name
@@ -105,23 +109,31 @@ class Ui(QtWidgets.QMainWindow):
         Function that is called when the download button is clicked,
         it is deactivated if the url is not valid
         """
-        self.folderpath = QtWidgets.QFileDialog.getExistingDirectory(self, "Select Folder")
+        self.folderpath = QtWidgets.QFileDialog.getExistingDirectory(
+            self, "Select Folder")
         # We get the Path where the user wants to download his file
         if self.folderpath != '':
             # We Check if the user hasn't closed the window for choosing his file
-            self.loadbar = ProgressBar(100, title=f"{self.titre_yt} download in progress ")
-            # We create an instance of the class ProgressBar. It is for showing the user how much time is left
-            self.video.register_on_progress_callback(self.progress_func)
-            # We say that, for each chunk of data retrieved,when downloading,we call that function
-            self.lastprogress = 0
-            # last percentage of the download => see below, that acts like a reset
             if self.audio_only.isChecked():
                 # if the checkbox "audio only" is checked
                 self.download_mp3()
             else:
                self.download_mp4()
 
-    def progress_func(self, stream, chunk, bytes_remaining):
+    def progress_func_mp3(self, stream, chunk, bytes_remaining):
+        """
+        Function call eatch time we retrive data from the download
+        """
+        total_size = stream.filesize
+        # we get the size of the video we want to download
+        bytes_downloaded = total_size - bytes_remaining
+        # we get the total of bytes downloaded
+        liveprogress = int(bytes_downloaded / total_size * 98)
+        # we make a percentage of that
+        self.loadbar.setValue(liveprogress)
+        # we update the progress bar
+
+    def progress_func_mp4(self, stream, chunk, bytes_remaining):
         """
         Function call eatch time we retrive data from the download
         """
@@ -131,68 +143,63 @@ class Ui(QtWidgets.QMainWindow):
         # we get the total of bytes downloaded
         liveprogress = int(bytes_downloaded / total_size * 100)
         # we make a percentage of that
-        if liveprogress > self.lastprogress:
-            # if the pourcentage is greater that the last percentage,
-            # is for making it less buggy and smoother
-            self.loadbar.setValue(liveprogress)
-            # we update the progress bar
-        self.lastprogress = liveprogress
-        # we add lastprogress in a var
-
-    def Thread_for_conversion(self):
-        """
-        It the Thread that is call for doing the conversion 
-        """
-        filePathMp4 = Path(os.getcwd(), "Temp", "FFMPEG_compatible.mp4")
-        filePathMp3 = Path(os.getcwd(), "Temp", "FFMPEG_compatible.mp3")
-        # we get the path of the file
-
-        ffmpeg = subprocess.call(["./ffmpeg/ffmpeg.exe", '-i', str(filePathMp4), str(filePathMp3)])
-        # we call subprocess for doing a command that will convert .mp4 audio file into .mp3 
-        
-        os.remove(filePathMp4)
-        # we delete the .mp4 file created in the first place
-        filePathMp3 = filePathMp3.rename(filePathMp3.with_name(str(self.titre_rename + ".mp3")))
-        # we rename the .mp3 file
-        shutil.move(filePathMp3, self.path_final.with_suffix('.mp3'))
-        # we move the file to the good directory
-        MessageBeep(-1)
-        # does a bip 
+        self.loadbar.setValue(liveprogress)
+        # we update the progress bar
 
     def download_mp3(self):
         """
         Function for downloading into .mp3
         """
-        if (self.titre_yt.isascii() == False) or (any(c in '/\:*?"<>|' for c in self.titre_yt) == True):
-            # if there are symbols not ascii or not compatible with window's filesystem
-            self.titre_rename = re.sub(r'[^\x00-\x7F]+', '', self.titre_yt)
-            # we removes non ascii characters
-            for char in '/\:*?"<>|':
-                self.titre_rename = self.titre_rename.replace(char, '')
-                # we removes "illegal" characters from filenames
-        else:
-            self.titre_rename = self.titre_yt
-            
-        self.titre_enregistrement = 'FFMPEG_compatible'
-        # we rename the file so that ffmpeg can use it
+        self.loadbar = ProgressBar(
+            100, title=f"{self.titre_yt} download in progress ")
+        # create a instance of the class for showing progress
+        # function call when we retrive a chunk of dat
+        self.video.register_on_progress_callback(self.progress_func_mp3)
 
-        where = Path(self.folderpath, self.titre_rename)
-        self.path_final = self.unique_filename(where, '.mp3')
-        self.titre_rename = self.path_final.name
-        # we see where the file will go at the end
+        # The name we will use to rename the file, witout bad characters
+        titre_rename = self.character_control(self.titre_yt)
+        titre_enregistrement = 'FFMPEG_compatible'  # name compatible with FFMPEG
 
-        yt_str = self.video.streams.filter(only_audio=True).first()
-        yt_str.download(Path(os.getcwd(), "Temp"), filename=self.titre_enregistrement)
+        path_folder_and_file_name = Path(self.folderpath, titre_rename)
+        path_final = self.unique_filename(path_folder_and_file_name, '.mp3')
+        titre_rename = path_final.name
+        # we get the final name for the file ; if on a folder they already have the file with the same name we had a number in front
+
+        yt_str = self.video.streams.filter(
+            only_audio=True, file_extension='mp4').first()
+        yt_str.download(self.path_temp, filename=titre_enregistrement)
         # We download the video by filtering all of the tracks, and we only keep the audio.
         # That saves the file in a .mp4 format into the Temp file
-        threading.Thread(target=self.Thread_for_conversion).start()
-        # we call a thread for converting the video into audio (convert .mp3 to .mp4), bc if the video is huge, 
-        # the process while take a while and halt the GUI . We move the file to the folder when finish 
+
+        # -----------------------conversion--------------------------------------------
+        filePathMp4 = Path(self.path_temp, "FFMPEG_compatible.mp4")
+        filePathMp3 = Path(self.path_temp, "FFMPEG_compatible.mp3")
+
+        # command for converting .mp4 to .mp3
+        ffmpeg = subprocess.call(
+            ["./ffmpeg/ffmpeg.exe", '-i', filePathMp4, filePathMp3])
+        self.loadbar.setValue(99)  # the download stop at 98%
+
+        os.remove(filePathMp4)  # we delete the .mp4 file created
+
+        filePathMp3 = filePathMp3.rename(filePathMp3.with_name(
+            titre_rename + ".mp3"))  # we rename the .mp3 file
+        # we move the file to the good directory
+        shutil.move(filePathMp3, self.folderpath)
+
+        self.loadbar.setValue(100)  # we show that the process had finish
+        MessageBeep(-1)
 
     def download_mp4(self):
         """
         Function for downloading in .mp4
         """
+
+
+<< << << < HEAD
+        self.video.register_on_progress_callback(self.progress_func_mp4)
+        # We say that, for each chunk of data retrieved,when downloading,we call that function
+== == == =
         if (self.titre_yt.isascii() == False) or (any(c in '/\:*?"<>|' for c in self.titre_yt) == True):
             # if there are symbols not ascii or not compatible with window's filesystem
             self.titre_rename = re.sub(r'[^\x00-\x7F]+', '', self.titre_yt)
@@ -202,17 +209,47 @@ class Ui(QtWidgets.QMainWindow):
                 # we removes "illegal" characters from filenames
         else:
             self.titre_rename = self.titre_yt
-        
-        where = Path(self.folderpath, self.titre_rename)
-        self.titre_rename = self.unique_filename(where, '.mp3').name
+>>>>>> > main
 
-        yt_str = self.video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
-        yt_str.first().download(self.folderpath, filename=self.titre_rename)
-            # We save the video by filtering the best quality video with the .mp4 format and download it.
-            # that save the file in a .mp4 format
+        titre_rename = self.character_control(self.titre_yt)
 
+        path_folder_and_file_name = Path(self.folderpath, titre_rename)
+        path_final = self.unique_filename(path_folder_and_file_name, '.mp4')
+        titre_rename = path_final.name
+
+        audio_stream = self.video.streams.filter(
+            adaptive=True, only_audio=True, file_extension='mp4').desc()
+        video_stream = self.video.streams.filter(
+            adaptive=True, res='1080p', file_extension='mp4').order_by('resolution').desc()
+        if len(video_stream) == 0:  # if no 1080
+            video_stream = self.video.streams.filter(
+                adaptive=True, file_extension='mp4').order_by('resolution').desc()
+
+        self.loadbar = ProgressBar(
+            100, title=f"{self.titre_yt} video download in progress ")
+        video_stream.first().download(self.path_temp, filename='video')
+        self.loadbar = ProgressBar(
+            100, title=f"{self.titre_yt} audio download in progress ")
+        audio_stream.first().download(self.path_temp, filename='audio')
+
+        # -----------------------merging--------------------------------------------
+        filePathVideo = Path(self.path_temp, 'video.mp4')
+        filePathAudio = Path(self.path_temp, 'audio.mp4')
+        filePathFinal = Path(self.path_temp, 'final.mp4')
+
+        subprocess.run(["./ffmpeg/ffmpeg.exe", '-i', filePathAudio,
+                       "-i", filePathVideo, '-c', 'copy', filePathFinal])
+
+        os.remove(filePathVideo)
+        os.remove(filePathAudio)
+        # we delete the .mp4 file created in the first place
+
+        filePathFinal = filePathFinal.rename(filePathFinal.with_name(
+            titre_rename + ".mp4"))  # we rename the .mp3 file
+        # we move the file to the good directory
+        shutil.move(filePathFinal, self.folderpath)
         MessageBeep(-1)
-        # does a bip 
+        # does a bip
 
     def unique_filename(self, output_filename, file_extension):
         """
@@ -230,9 +267,24 @@ class Ui(QtWidgets.QMainWindow):
         """
         Reset the temp file
         """
-        shutil.rmtree(Path(os.getcwd(), "Temp"))
-        os.mkdir(Path(os.getcwd(), "Temp"))
+        shutil.rmtree(self.path_temp)
+        os.mkdir(self.path_temp)
 
+    def character_control(self, world):
+        """
+        Return a title for the video without character that will broke the app
+        """
+        if (world.isascii() == False) or (any(c in '/\:*?"<>|' for c in world) == True):
+            # if there are symbols not ascii or not compatible with window's filesystem
+            titre_rename = re.sub(r'[^\x00-\x7F]+', '', world)
+            # we removes non ascii characters
+            for char in '/\:*?"<>|':
+                titre_rename = titre_rename.replace(char, '')
+                # we removes "illegal" non window characters from filenames
+        else:
+            titre_rename = world
+        
+        return titre_rename
 
 
 class ProgressBar(QProgressDialog):
